f438c85e0ee422b0b62c77998c0d33429a8905e1
Jose David <lujan9495@gmail.com>
2025-07-07 09:07:01 -0500

    Included a header and main section in Layout component

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -3,8 +3,15 @@ import { Outlet } from "react-router-dom"
 export default function Layout() {
   return (
     <>
-      <div>Desde el layout</div>
-      <Outlet />
+      <header className="bg-slate-800">
+        <div className="mx-auto max-w-6xl py-10">
+          <h1 className="text-4xl font-bold text-white">Product Manager</h1>
+        </div>
+      </header>
+
+      <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
+        <Outlet />
+      </main>
     </>
   )
 }
