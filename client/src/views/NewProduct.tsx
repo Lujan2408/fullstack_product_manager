5b85d3fa4322782d0f66ea6fdbc620bd09d923aa
Jose David <lujan9495@gmail.com>
2025-07-07 09:15:48 -0500

    Routing between pages:
    -Enhance NewProduct and Products views with headers and navigation links for improved user experience

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function NewProduct() {
   return (
-    <div>NewProduct</div>
-  )
+    <>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-black text-slate-500">Add New Product</h1>
+        <Link
+          to="/"
+          className="bg-indigo-600 text-white p-3 rounded-md font-medium shadow-sm hover:bg-indigo-500"
+        >
+          Back to Products
+        </Link>
+      </div>
+    </>
+  );
 }
--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-black text-slate-500">Products</h1>
+        <Link 
+          to="/products/new" 
+          className="bg-indigo-600 text-white p-3 rounded-md font-medium shadow-sm hover:bg-indigo-500"
+        >
+          New Product
+        </Link>
+      </div>
+    </>
   )
 }
