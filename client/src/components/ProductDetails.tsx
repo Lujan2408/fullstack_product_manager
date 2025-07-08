65afdf7f562310ad0d5cf043a9aa940c03155125
Jose David <lujan9495@gmail.com>
2025-07-08 13:41:27 -0500

    Enhance ProductDetails component with navigation for editing product.  Integrated useNavigate hook to enable navigation to the edit view of a product

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router"
 import { type Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -6,6 +7,9 @@ type ProductDetailsProps = {
 }
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
+  
+  const navigate = useNavigate()
+  
   return (
     <tr className="border-b border-slate-300 font-medium">
       <td className="p-3 text-lg text-gray-800">{product.name}</td>
@@ -13,9 +17,12 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
       <td className="p-3 text-lg text-gray-800">{product.available ? 'Available' : 'No Available'}</td>
       <td className="p-3 text-lg text-gray-800 ">
         <div className="flex gap-2 items-center justify-center"> 
-          <button className="bg-blue-600 text-white px-4 py-1 rounded-md cursor-pointer hover:bg-blue-500 uppercase">Edit</button>
+          <button 
+            onClick={() => navigate(`/products/${product.id}/edit`)}
+            className="bg-indigo-600 text-white p-2 rounded-lg text-xs cursor-pointer w-full hover:bg-indigo-500 uppercase"
+          >Edit</button>
         </div>
-      </td>
+      </td> 
     </tr>
   );
 }
